5b7ee5e19fd76e4b5cf5efe1c94c8852f4b9b99b
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-07-31 00:56:11 -0400

    update documentation
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- ping.py
+++ ping.py
@@ -2,23 +2,8 @@ from errbot import BotPlugin, botcmd
 
 
 class Ping(BotPlugin):
-    """
-    Am I alive?
-    It's too early in the morning for such thoughts.
-
-    ...in the meantime, though...
-
-    >>> user: ping
-    >>> errbot: PONG!
-    """
 
     @botcmd
     def ping(self, msg, args):
-        """
-        PING!
-
-        :param msg: Message object
-        :param args: all words after the initial command
-        :return: "PONG!"
-        """
+        """PONG!"""
         return 'PONG!'
